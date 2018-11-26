c70f980bd7a74f723ffcef002d42d077490f1d9d
Niels van den Hork <hhork@hg023pc39.science.ru.nl>
2018-11-26 15:14:57 +0100

    16-11-end

--- code.py
+++ code.py
@@ -8,13 +8,18 @@ import random
 import math
 from operator import itemgetter
 import nltk
+
+from nltk.corpus import wordnet
 from nltk.stem.snowball import SnowballStemmer
 from nltk.tokenize import wordpunct_tokenize, sent_tokenize, word_tokenize
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, brown
 
 nltk.download('stopwords')
 nltk.download('punkt')
+nltk.download('wordnet')
 PLOTPATH = 'plots'
+
+freqs = nltk.FreqDist(w.lower() for w in brown.words())
 
 import os
 if not os.path.exists(PLOTPATH):
@@ -64,28 +69,49 @@ def plot_averages(plain,our):
 
 def transform(query):
 
-    stop_words = set(stopwords.words('english'))
-    word_tokens = word_tokenize(query)
-    tokens = [w for w in word_tokens if not w in stop_words]
-    
-    modified_query = ""
-    for word in tokens:
-        modified_query += " " + word #stemmer.stem(word)
+	stop_words = set(stopwords.words('english'))
+	word_tokens = word_tokenize(query)
+	tokens = [w for w in word_tokens if not w in stop_words]
 
-    #print("MODIFIED QUERY ", modified_query)
-    return modified_query
+	
+
+	modified_query = ""
+	for word in tokens:
+		modified_query += " " + word #stemmer.stem(word)
+
+	#modified_query = "".join([char for char in modified_query if char not in ['.',',','?','!']])
+	#no difference
+
+	for word in modified_query.split(" "):
+		syns = wordnet.synsets(word)
+
+		if (syns):
+			syns = list(set([(freqs[syn.lemmas()[0].name().lower()] ,syn.lemmas()[0].name()) for syn in syns]))
+			syns.append((freqs[word.lower()] ,word) )
+			syns = sorted(syns, key = lambda x : -x[0]) #-freqs[x[1]])
+			#print(syns)
+
+
+
+			modified_query +=  " " + syns[0][1]
+			#print(syns,word)
+    
+	# decrease performance by 4%
+
+	#print("MODIFIED QUERY ", modified_query)
+	return modified_query
 
 def get_relevance(query):
 	#Compare nordlys with relevance
-	with open (drive + "queries-v2.txt", 'r') as queries:
+	with open (drive + "queries-NLP.txt", 'r') as queries:
 		for row in queries:
 			if query in row:
 				code = row.split("\t")[0]
-
+	
 	DB_rels = []
 	with open (drive + "qrels-v2.txt", 'r') as qrels: #, encoding='utf-8'
 		for row in qrels:
-			if code in row:
+			if code == row.split("\t")[0]:
 				DB_rels.append(row)
 
 	return DB_rels
@@ -100,6 +126,7 @@ def calc_ndcg(score):
 		# print(sorted_score)
 		idcg = sorted_score[0][1]
 		for i in range(1, len(score)):
+			#print(score[i][0])
 			dcg += score[i][1]/math.log(score[i][0])
 			idcg += sorted_score[i][1]/math.log(score[i][0])
 
@@ -109,60 +136,64 @@ def calc_ndcg(score):
 
 
 def evaluate(query,original_query = None): #,relevance):
-    if(not original_query):
-        original_query = query
+	if(not original_query):
+		original_query = query
 
-    #Query nordlys
-    #REST API documentation https://nordlys.readthedocs.io/en/latest/restful_api.html
-    base_url = 'http://api.nordlys.cc/'
-    parameters = 'er?1st_num_docs=20&model=lm&q='
+	#Query nordlys
+	#REST API documentation https://nordlys.readthedocs.io/en/latest/restful_api.html
+	base_url = 'http://api.nordlys.cc/'
+	parameters = 'er?1st_num_docs=20&model=lm&q='
 
-    response = requests.get(base_url + parameters + query)
-    if response.status_code != 200:
-    	raise ApiError('GET /er?{}/ {}'.format(parameters + query, response.status_code))
+	response = requests.get(base_url + parameters + query)
+	if response.status_code != 200:
+		raise ApiError('GET /er?{}/ {}'.format(parameters + query, response.status_code))
 
-    #Access individual result
-    #Example json data: http://api.nordlys.cc/er?1st_num_docs=20&model=lm&q=Amsterdam
-    results = response.json()['results']
-    entities = []
-    for i in results:
-    	result = results[i]
-    	entities.append(result['entity'])
-    	# print('{} {}'.format(result['entity'], result['score']))
+	#Access individual result
+	#Example json data: http://api.nordlys.cc/er?1st_num_docs=20&model=lm&q=Amsterdam
+	results = response.json()['results']
+	entities = []
+	for i in results:
+		result = results[i]
+		entities.append(result['entity'])
+		# print('{} {}'.format(result['entity'], result['score']))
 
-    score_pos = []
-    DB_rels = get_relevance(original_query)
-    for i in range(len(entities)):
-    	for rel in DB_rels:
-    		if entities[i] in rel:
-    			score = int(rel[-2:-1])
-    			score_pos.append((i+1, score))
-    			#print(score_pos[-1])
+	score_pos = []
+	DB_rels = get_relevance(original_query)
+	#print(DB_rels)
+	for i in range(len(entities)):
+		for rel in DB_rels:
+			if entities[i] in rel:
+				#print(rel)
+				score = int(rel[-2:-1])
+				score_pos.append((i+1, score))
+	#print(score_pos)
+		
 
-    ndcg = calc_ndcg(score_pos)
-    #print("Query: " + query + " \tNDCG = {:f}".format(ndcg))
-    return query, ndcg
+	ndcg = calc_ndcg(score_pos)
+	#print("Query: " + query + " \tNDCG = {:f}".format(ndcg))
+	return query, ndcg
 
 
 def main():
-    #Load query and relevance data
-    with open (drive + "queries-NLP.txt", 'r') as query_files:
-        queries = [q.split("\t")[1][:-1] for q in query_files]
-        
-    print(queries)
-    relevance = []
-    #queries = ["Who is the mayor of Berlin?"]
-    oscores,scores = [],[]
-    for query in queries:
-        #print(query)
-        _,oscore = evaluate(query)
+	#Load query and relevance data
+	with open (drive + "queries-NLP.txt", 'r') as query_files:
+		queries = [q.split("\t")[1][:-1] for q in query_files]
 
-        modified = transform(query)
-        _,score = evaluate(modified,query)
-        scores.append(score)
-        oscores.append(oscore)
-        #print(str(oscore) +" "+ str(sum(oscores)/len(oscores)) + '\t' + str(score) + ' '+ str(sum(scores)/len(scores)) +' \t '+ query  )
-        print("Original:" + str(sum(oscores)/len(oscores)) + "    Modified: " + str(sum(scores)/len(scores)) +' \t '+ query  )
+	#print(queries)
+	relevance = []
+	#queries = ["Which river does the Brooklyn Bridge cross?"]
+	oscores,scores = [],[]
+	for query in queries:
+		#print(query)
+		_,oscore = evaluate(query)
+
+		modified = transform(query)
+		_,score = evaluate(modified,query)
+		scores.append(score)
+		oscores.append(oscore)
+		#print(str(oscore) +" "+ str(sum(oscores)/len(oscores)) + '\t' + str(score) + ' '+ str(sum(scores)/len(scores)) +' \t '+ query  )
+		#print("Original:" + str(sum(oscores)/len(oscores))[:6] + "    Modified: " + str(sum(scores)/len(scores))[:6] +' \t '+ query  )
+		print("increase:" + str(sum(scores)/len(scores) - sum(oscores)/len(oscores))[:6] + ' \t '+ query  )
 
 if __name__ == "__main__":
     main()
